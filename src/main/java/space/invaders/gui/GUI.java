61cc4f55c4b73f6720152ff5dcfee1ba4e5f6de1
Heidi Mork <heidimork@yahoo.no>
2019-03-03 17:24:33 +0100

    rename bulletdto type to sender

--- src/main/java/space/invaders/gui/GUI.java
+++ src/main/java/space/invaders/gui/GUI.java
@@ -85,7 +85,7 @@ public class GUI extends AbstractActor {
         bullet.setCenterX(bulletDto.posX);
         bullet.setCenterY(bulletDto.posY);
         bullet.setRadius(2.0);
-        String style = bulletDto.type.equals(BulletDto.Type.Player) ? "player-bullet" : "alien-bullet";
+        String style = bulletDto.sender.equals(BulletDto.Sender.Player) ? "player-bullet" : "alien-bullet";
         bullet.getStyleClass().add(style);
         return bullet;
     }
