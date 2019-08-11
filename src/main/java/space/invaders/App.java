faca152147527728a7f76905dd5699641e593d3f
Heidi Mork <heidimork@yahoo.no>
2019-08-11 21:15:54 +0200

    Remote game application

--- src/main/java/space/invaders/App.java
+++ src/main/java/space/invaders/App.java
@@ -2,13 +2,12 @@ package space.invaders;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
-import space.invaders.gui.GUI;
 
 public class App {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("space-invaders");
-        ActorRef gui = system.actorOf(GUI.props().withDispatcher("javafx-dispatcher"), "gui");
+        //ActorRef gui = system.actorOf(GUI.props().withDispatcher("javafx-dispatcher"), "gui");
         ActorRef gameInitializer = system.actorOf(GameInitializer.props(), "game-initializer");
-        gameInitializer.tell(new GameInitializer.Initialize(gui, "player1"), ActorRef.noSender());
+        //gameInitializer.tell(new GameInitializer.Initialize(gui, "player1"), ActorRef.noSender());
     }
 }
